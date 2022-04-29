a8b2ddebbcdadf10f6001d1c73f5469e5afe6f6d
Yan Musikhin <yanmusikhin@gmail.com>
2022-04-29 08:53:45 +0400

    init branch

---
+++ assets/images/icons/arrow.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+export const Arrow = ({color="#C7C7CC"}) => (
+  <Svg width="6" height="8" fill="none">
+    <Path
+      d="M5.94 4.12 1.2 7.64l-.34-.42 2.61-3.1L.86.58 1.2.3l4.74 3.82Z"
+      fill={color}
+    />
+  </Svg>
+);
