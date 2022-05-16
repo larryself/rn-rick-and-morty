fb0ffdb8d85a5cc39419e1430ad8e6ca41426823
Yan Musikhin <yanmusikhin@gmail.com>
2022-05-16 17:00:38 +0400

    fix: edit code style

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import { NavigationContainer } from '@react-navigation/native';
+import { ErrorBoundary } from 'src/errorBoundary';
+import { client } from 'src/graphql/client';
 import { RootNavigation } from 'src/navigation/root';
 import { store } from './store';
 
-const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql',
-  cache: new InMemoryCache(),
-});
-
 export const App = () => {
   return (
-    <Provider store={store}>
-      <ApolloProvider client={client}>
-        <NavigationContainer>
-          <RootNavigation />
-        </NavigationContainer>
-      </ApolloProvider>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <ApolloProvider client={client}>
+          <NavigationContainer>
+            <RootNavigation />
+          </NavigationContainer>
+        </ApolloProvider>
+      </Provider>
+    </ErrorBoundary>
   );
 };
