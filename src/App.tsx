8c1a529472f76e7db077b01429c11df468adcb42
Yan Musikhin <yanmusikhin@gmail.com>
2022-06-03 16:15:19 +0400

    fix: del redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { ApolloProvider } from '@apollo/client';
 import { NavigationContainer } from '@react-navigation/native';
 import { ErrorBoundary } from 'src/errorBoundary';
-import { client } from 'src/graphql/client';
+import { client } from 'src/graphql/client/client';
 import { RootNavigation } from 'src/navigation/root';
-import { store } from './store';
 
 export const App = () => {
   return (
     <ErrorBoundary>
-      <Provider store={store}>
-        <ApolloProvider client={client}>
-          <NavigationContainer>
-            <RootNavigation />
-          </NavigationContainer>
-        </ApolloProvider>
-      </Provider>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigation />
+        </NavigationContainer>
+      </ApolloProvider>
     </ErrorBoundary>
   );
 };
